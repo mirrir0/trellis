ce4d51efce5a007d64182d258984cddaf8783be1
M B <mirrir@mirrir.net>
2023-11-07 17:28:05 -0700

    (feat): added leveled logging utility

---
+++ go/0kn/pkg/utils/log.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"encoding/json"
+	"go.uber.org/zap"
+)
+
+func InitLogging() *zap.Logger {
+	// TODO: get this from a config file
+	rawJSON := []byte(`{
+	  "level": "debug",
+	  "encoding": "json",
+	  "outputPaths": ["stdout", "/tmp/logs"],
+	  "errorOutputPaths": ["stderr"],
+	  "initialFields": {"foo": "bar"},
+	  "encoderConfig": {
+	    "messageKey": "message",
+	    "levelKey": "level",
+	    "levelEncoder": "lowercase"
+	  }
+	}`)
+
+	var cfg zap.Config
+	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
+		panic(err)
+	}
+	logger := zap.Must(cfg.Build())
+	defer logger.Sync()
+
+	logger.Info("logger initialization succeeded")
+	return logger
+}
