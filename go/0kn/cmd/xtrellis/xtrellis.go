cfe582d6145006a3e49df09752ed693def4539a8
M B <mirrir@mirrir.net>
2023-11-21 10:24:41 -0700

    rebasing

--- go/0kn/cmd/xtrellis/xtrellis.go
+++ go/0kn/cmd/xtrellis/xtrellis.go
@@ -10,88 +10,9 @@ import (
 	"github.com/31333337/bmrng/go/0kn/pkg/utils"
 )
 
-// from cmd/server/server.go
-func LaunchServer(args ArgsServer) {
-	serversFile := args.ServerFile
-	groupsFile := args.GroupFile
-	addr := args.Addr
-	errors.Addr = addr
-
-	logger := utils.InitLogging()
-	sugar := logger.Sugar()
-	defer sugar.Sync()
-	sugar.Infow(
-		"Launching server",
-		"address %s", addr,
-	)
-
-	servers, err := config.UnmarshalServersFromFile(serversFile)
-	if err != nil {
-		sugar.Fatalf("Could not read servers file %s", serversFile)
-	}
-
-	groups, err := config.UnmarshalGroupsFromFile(groupsFile)
-	if err != nil {
-		sugar.Fatalf("Could not read group file %s", groupsFile)
-	}
-
-	// will start in blocked state
-	h := server.NewHandler()
-	server := server.NewServer(&config.Servers{Servers: servers}, &config.Groups{Groups: groups}, h, addr)
-
-	server.TcpConnections.LaunchAccepts()
-	network.RunServer(h, server, servers, addr)
-	config.Flush()
-}
-
-// from cmd/client/client.go
-func LaunchClient(args ArgsClient) {
-	serversFile := args.ServerFile
-	groupsFile := args.GroupFile
-	clientsFile := args.ClientFile
-	addr := args.Addr
-	errors.Addr = addr
-
-	logger := utils.InitLogging()
-	sugar := logger.Sugar()
-	defer sugar.Sync()
-	sugar.Infow(
-		"Launching client",
-		"address %s", addr,
-	)
-
-	servers, err := config.UnmarshalServersFromFile(serversFile)
-	if err != nil {
-		log.Fatalf("Could not read servers file %s", serversFile)
-  }
-
-	groups, err := config.UnmarshalGroupsFromFile(groupsFile)
-	if err != nil {
-		log.Fatalf("Could not read group file %s", groupsFile)
-	}
-
-	clients, err := config.UnmarshalServersFromFile(clientsFile)
-	if err != nil {
-		log.Fatalf("Could not read clients file %s", clientsFile)
-	}
-
-	clientRunner := client.NewClientRunner(servers, groups)
-
-	// This fails @ network/rpc_call.go callee.HandleSignedMessageStream
-	/*
-		err = clientRunner.Connect()
-		if err != nil {
-			log.Fatalf("Could not make clients %v", err)
-		}
-	*/
-
-	network.RunServer(nil, clientRunner, clients, addr)
-}
-
-// set the working directory from env var and change to the directory
 func setWorkingDirectory() {
 	// get working directory from env var, ensure set for children processes
-  workingDir := getWorkingDirectory()
+	workingDir := os.Getenv("_0KN_WORKDIR")
 	if workingDir == "" {
 		homeDir, err := os.UserHomeDir()
 		if err != nil {
@@ -132,29 +53,11 @@ func main() {
 	utils.SetDebugLogEnabled(args.Debug)
 	utils.SetDebugLogCallerEnabled(args.DebugCaller)
 
-	logger := utils.InitLogging()
-
-	////////////////////////////////////////////////////////////////////////
-	// change current working directory for trellis hardcoded paths TODO
-	////////////////////////////////////////////////////////////////////////
-	// TODO: handle this cleanly with a config file(?)
-	trellisCoordinatorDirectory := "../../../trellis/cmd/coordinator/"
-	if err := os.Chdir(trellisCoordinatorDirectory); err != nil {
-		sugar := logger.Sugar()
-		sugar.Fatalw(
-			"Failed to change the working directory",
-			"error: %v", err,
-		)
-		sugar.Sync()
-		return
-	}
-
 	setWorkingDirectory()
-
 
 	switch {
 	case args.Coordinator != nil:
-		LaunchCoordinator(*args.Coordinator, argParser, logger)
+		LaunchCoordinator(*args.Coordinator, argParser)
 
 	case args.Client != nil:
 		LaunchClient(*args.Client)
